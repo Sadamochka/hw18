3987c93111f529eba71533cad5f64d5be249587f
Your Name <you@example.com>
2023-08-20 00:14:16 +0300

    added hw18

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const Timer = () => {
+  const [time, setTime] = useState(0);
+  const [inputTime, setInputTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning && time > 0) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime - 1);
+      }, 1000);
+    }
+
+    if (time === 0) {
+      setIsRunning(false);
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunning, time]);
+
+  const startTimer = () => {
+    setTime(inputTime);
+    setIsRunning(true);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Timer App</h1>
+      <div>
+        <label>Enter time in seconds:</label>
+        <input
+          type="number"
+          value={inputTime}
+          onChange={e => setInputTime(parseInt(e.target.value))}
+        />
+      </div>
+      <div>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+      </div>
+      <div>
+        <p>Time remaining: {time} seconds</p>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Timer;
